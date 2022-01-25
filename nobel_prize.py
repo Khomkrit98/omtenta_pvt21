b2ba1b34760f2768c6123741ea2fe72356f81eea
Khomkrit98 <91267744+Khomkrit98@users.noreply.github.com>
2022-01-25 09:32:38 +0100

    Update nobel_prize.py

--- nobel_prize.py
+++ nobel_prize.py
@@ -3,7 +3,8 @@ import requests
 
 # Tips: använd sidan nedan för att se vilken data vi får tillbaks och hur apiet fungerar
 # vi använder oss enbart av /nobelPrizes
-# Dokumentation, hjälp samt verktyg för att testa apiet fins här: https://app.swaggerhub.com/apis/NobelMedia/NobelMasterData/2.1
+# Dokumentation, hjälp samt verktyg för att testa apiet fins här:
+# https://app.swaggerhub.com/apis/NobelMedia/NobelMasterData/2.1
 
 HELP_STRING = """
 Ange ett år och fält
@@ -13,12 +14,12 @@ Ange Q för att avsluta programmet
 Ange H för att  skriva ut Hjälp texten
 """
 
-cat = {"fysik": "phy",
-       "kemi": "che",
-       "litteratur": "lit",
-       "ekonomi": "eco",
-       "fred": "pea",
-       "medicin": "med"}
+fields = {"fysik": "phy",
+          "kemi": "che",
+          "litteratur": "lit",
+          "ekonomi": "eco",
+          "fred": "pea",
+          "medicin": "med"}
 
 
 # TODO 10p programmet skall ge en hjälpsam utskrift istället för en krasch om användaren skriver in fel input TODO
@@ -30,34 +31,34 @@ def main():
     print(HELP_STRING)
     while True:
         # TODO 5p Gör så att hjälptexten skrivs ut om användaren skriver h eller H
-        thy_command = input(">")
-        if thy_command.upper() == "Q":
+        user_input = input(">")
+        if user_input.upper() == "Q":
             break
-        if thy_command.upper() == "H":
+        if user_input.upper() == "H":
             print(HELP_STRING)
-        a, b = thy_command.split()
-        c = cat[b]
+        years, field = user_input.split()
+        category = fields[field]
 
-        c = {"nobelPrizeYear": int(a), "nobelPrizeCategory": c}
+        category = {"nobelPrizeYear": int(years), "nobelPrizeCategory": category}
 
-        res = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=c).json()
+        data_from_api = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=category).json()
         # TODO 5p  Lägg till någon typ av avskiljare mellan pristagare, exempelvis --------------------------
-        print_prize(res)
+        print_prize(data_from_api)
         # TODO 15p Skriv ut hur mycket pengar varje pristagare fick, tänk på att en del priser delas mellan flera mottagare, skriv ut både i dåtidens pengar och dagens värde
         #   Skriv ut med tre decimalers precision. exempel 534515.123
         # Feynman fick exempelvis 1/3 av priset i fysik 1965, vilket borde gett ungefär 282000/3 kronor i dåtidens penningvärde
 
 
 def print_prize(res):
-    for p in res["nobelPrizes"]:
-        peng = p["prizeAmount"]
-        idagpeng = p["prizeAmountAdjusted"]
-        print(f"{p['categoryFullName']['se']} prissumma {peng} SEK")
+    for prize in res["nobelPrizes"]:
+        prize_amount = prize["prizeAmount"]
+        # idagpeng = prize["prizeAmountAdjusted"]
+        print(f"{prize['categoryFullName']['se']} prissumma {prize_amount} SEK")
 
-        for m in p["laureates"]:
-            print(m['knownName']['en'])
-            print(m['motivation']['en'])
-            andel = m['portion']
+        for recipient in prize["laureates"]:
+            print(recipient['knownName']['en'])
+            print(recipient['motivation']['en'])
+            # andel = recipient['portion']
 
 
 if __name__ == '__main__':
